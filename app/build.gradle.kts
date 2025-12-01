ab44bcb2a8ee4548439890038f7a2388b1e0eb74
Anuj Singh <anuj1112131415@gmail.com>
2025-12-01 15:54:54 +0530

    fix ui

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.anujsinghdev.todo"
         minSdk = 28
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
