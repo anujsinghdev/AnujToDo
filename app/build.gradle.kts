ce0ebaf21572445f2144508c43466274bebd0766
Anuj Singh <anuj1112131415@gmail.com>
2025-12-02 21:03:00 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.anujsinghdev.todo"
         minSdk = 28
         targetSdk = 36
-        versionCode = 2
-        versionName = "2.0"
+        versionCode = 3
+        versionName = "3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
